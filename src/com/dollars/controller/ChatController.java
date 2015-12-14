50cf6a492fcdbbc74be6c83fd33c0989ae35d721
TomChen <cwdtom@yahoo.com>
2015-12-14 09:28:45 +0800

    jre改为1.7
    Signed-off-by: TomChen <cwdtom@yahoo.com>

---
+++ src/com/dollars/controller/ChatController.java
@@ -0,0 +1,13 @@
+package com.dollars.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ChatController {
+	
+	@RequestMapping("/chat.do")
+	public String chat(){
+		return "chat";
+	}
+}
